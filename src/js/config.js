a0b1c2fc990cb8915ce7793e8679f9e65e55d528
Mohamed Hassan <mohamedahassan188@gmail.com>
2021-03-26 20:09:38 +0000

    initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '1869e80b-187f-4911-a832-b02db9a5ddd2';
+export const MODAL_CLOSE_SEC = 2.5;
